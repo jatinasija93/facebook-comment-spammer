6e026d296885f07e03f3b0f0344f81d092eb8da2
jatinasija93 <jatinasija1@gmail.com>
2017-07-17 00:26:03 +0530

    FB_CommentSpam.py
    1.Changed Limit to 100 as 250 wasn't working.
    2.Made Changes according to Python 3.0.

--- FB_commentSpam.py
+++ FB_commentSpam.py
@@ -3,12 +3,12 @@ import fb                     #To install this package run: sudo pip install fb
 from facepy import GraphAPI   #To install this package run: sudo pip install facepy
 
 def spam():
-    token = "" #Insert access token here.  
+    token = "" #Insert access token here.
     facebook = fb.graph.api(token)
     graph1 = GraphAPI(token)
     
     vid = input("Enter victim's Facebook id: ")
-    query = str(vid) + "/posts?fields=id&limit=250"
+    query = str(vid) + "/posts?fields=id&limit=100"
     r = graph1.get(query)
     
     
@@ -17,13 +17,13 @@ def spam():
     idlist.reverse()
     print("There are "+ str(len(idlist)) +" spammable posts.")
     
-    char1 = raw_input("Do you want to spam? (y/n) ")
+    char1 = input("Do you want to spam? (y/n) ")
     count = 0
     if char1 == 'y':
         nos = input("Enter number of posts to be spammed with comments: ")
-        mess = raw_input("Enter the message to be commented: ")
-        if nos <= len(idlist):
-           for indid in (idlist[(len(idlist) - nos):]):
+        mess = input("Enter the message to be commented: ")
+        if int(nos) <= len(idlist):
+           for indid in (idlist[(len(idlist) - int(nos)):]):
             facebook.publish(cat = "comments", id = indid, message = mess) #Comments on each post
             facebook.publish(cat = "likes", id = indid) #Likes each post
             count += 1
